00f88fed26eea87714304a0b70f45cb5739ae669
ash-heinz <ashwast.gupta@gmail.com>
2026-01-12 23:19:07 +0530

    DAY 12

---
+++ DAY 12/code.py
@@ -0,0 +1,29 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        
+        def binary_search(nums, target, searchl):
+            l = 0
+            r = len(nums) - 1
+            idx = -1
+            
+            while l <= r:
+                mid = (l + r) // 2
+                
+                if nums[mid] > target:
+                    r = mid - 1
+                elif nums[mid] < target:
+                    l = mid + 1
+                else:
+                    idx = mid
+                    if searchl:
+                        r = mid - 1
+                    else:
+                        l = mid + 1
+            
+            return idx
+        
+        l = binary_search(nums, target, True)
+        r = binary_search(nums, target, False)
+        
+        return [l, r]
+            
